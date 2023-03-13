18e6beef3c77e4725ec55bec9f11fed5b6e209b2
lpoto <luka.potochnik@gmail.com>
2023-03-13 02:07:23 +0100

    Use multiple datastore variables instead of just url

--- src/datastore.rs
+++ src/datastore.rs
@@ -2,7 +2,6 @@ use std::{env, ops::DerefMut};
 
 use deadpool_postgres::{Config, ManagerConfig, Pool, RecyclingMethod};
 use tokio_postgres::NoTls;
-use url::Url;
 
 pub mod user;
 
@@ -20,32 +19,17 @@ impl Datastore {
     /// when the pool could not be created.
     pub fn new() -> Datastore {
         let mut dp_config = Config::new();
-        let database_url = env::var("DATABASE_URL")
-            .expect("missing DATABASE_URL env variable");
-        let url = Url::parse(database_url.as_str())
-            .expect("invalid DATABASE_URL env variable");
-        let user = url.username().to_string();
-        let password = url
-            .password()
-            .ok_or("")
-            .expect("invalid password in DATABASE_URL")
-            .to_string();
-        let host = url
-            .host()
-            .ok_or("")
-            .expect("invalid host in DATABASE_URL")
-            .to_string();
-        let port = url.port().ok_or("").expect("invalid port in DATABASE_URL");
-        let dbname = url
-            .path_segments()
-            .expect("invalid database name in DATABASE_URL")
-            .next()
-            .unwrap()
-            .to_string();
+        let dbname =
+            env::var("POSTGRES_DB").expect("missing DATABASE_URL env variable");
+        let host = env::var("POSTGRES_HOST")
+            .expect("missing POSTGRES_HOST env variable");
+        let user = env::var("POSTGRES_USER")
+            .expect("missing POSTGRES_USER env variable");
+        let password = env::var("POSTGRES_PASSWORD")
+            .expect("missing POSTGRES_PASSWORD env variable");
 
         dp_config.host = Some(host);
         dp_config.user = Some(user);
-        dp_config.port = Some(port);
         dp_config.password = Some(password);
         dp_config.dbname = Some(dbname.clone());
         dp_config.manager = Some(ManagerConfig {
